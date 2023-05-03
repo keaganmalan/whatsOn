3eec11ac988c36ca9c6e82636d740e36484b95d2
Nivek Ranjith <nivek@wimtechnologies.com>
2023-05-03 20:03:41 +0200

    third push

---
+++ WhatsOn/ViewModels/MapViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms.GoogleMaps;
+
+namespace WhatsOn.ViewModels
+{
+    public class MapViewModel: BaseViewModel
+    {
+        public MapViewModel()
+        {
+        }
+      
+    }
+}
